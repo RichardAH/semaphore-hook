71e8a27c7740a1b21f4886b5e54b8266a5d307ad
Richard Holland <richard.holland@starstone.co.nz>
2025-06-25 20:54:53 +1000

    add threshold

--- sem.c
+++ sem.c
@@ -21,7 +21,8 @@ int64_t hook(uint32_t r)
     
     uint8_t semaphore = 0;
     state(SVAR(semaphore), "SEM", 3);
-
+    uint64_t semamt = 0;
+    uint64_t has_amt = (state(SVAR(semamt), "SEMAMT", 6) == 8);
     uint64_t is_setup = (state(SBUF(sigacc), "SEMACC", 6) == 20);
 
     if (BUFFER_EQUAL_20(hookacc, otxnacc))
@@ -32,14 +33,40 @@ int64_t hook(uint32_t r)
 
         // use invoke to set signalling account
 
-        if (otxn_param(SBUF(sigacc), "SEMACC", 6) != 20)
-            NOPE("Sem: self ttINVOKE did not contain SEMACC parameter.");
-    
-        // set the signal account
-        if (state_set(SBUF(sigacc), "SEMACC", 6) != 20)
-            NOPE("Sem: could not set SEMACC hook state.");
+        uint64_t params_set = 0;
 
-        DONE("Sem: new SEMACC set.");
+        int64_t ret = otxn_param(SBUF(sigacc), "SEMACC", 6);
+        if (ret == 20)
+        {
+            // set the signal account
+            if (state_set(SBUF(sigacc), "SEMACC", 6) != 20)
+                NOPE("Sem: could not set SEMACC hook state.");
+
+            params_set++;
+        }
+        else if (ret > 0)
+            NOPE("Sem: specified SEMACC but wrong size, expecting 20 bytes.");
+        
+        uint64_t sigamt = 0;
+        ret = otxn_param(SVAR(sigamt), "SEMAMT", 6);
+        if (ret == 8)
+        {
+            if (state_set(SVAR(sigamt), "SEMAMT", 6) != 8)
+                NOPE("Sem: could not set SEMAMT hook state.");
+            
+            params_set++;
+        }
+        else if (ret > 0)
+            NOPE("Sem: specified SEMAMT but wrong size, expecting 8 bytes LE int");
+
+        if (params_set > 0)
+        {
+            DONE("Sem: param(s) set.");
+        }
+        else
+        {
+            NOPE("Sem: specify either or both of SEMACC (20 bytes) and SEMAMT (8 bytes le uint).");
+        }
     }
 
     if (!is_setup)
@@ -50,9 +77,32 @@ int64_t hook(uint32_t r)
         // check if the invoke is from the signalling account
         if (BUFFER_EQUAL_20(otxnacc, sigacc))
         {
-            // flip the semaphore
-            semaphore++;
-            semaphore %= 2;
+            uint8_t dummy;
+            uint64_t bal;
+            uint64_t has_bal = (otxn_param(SVAR(bal), "BAL", 3) == 8);
+
+            if (otxn_param(SVAR(dummy), "BLOCK", 5) > 0)
+                semaphore = 1;
+            else if (otxn_param(SVAR(dummy), "ALLOW", 5) > 0)
+                semaphore = 0;
+            else if (has_bal)
+            {
+                if (!has_amt)
+                    semaphore = 0;
+                else
+                {
+                    // flip sem based on balance threshold
+                    semaphore = !(bal < semamt);
+                } 
+            }
+            else
+            {
+                // blank invoke flips the semaphore
+                semaphore++;
+                semaphore %= 2;
+            }
+            
+
             if (state_set(SVAR(semaphore), "SEM", 3) < 0)
                 NOPE("Sem: could not set new semaphore value.");
 
